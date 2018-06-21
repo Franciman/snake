436e4903ad1b0338910729de1897827c7c6a1a67
Francesco Magliocca <franciman12@gmail.com>
2018-06-21 20:33:55 +0200

    Take back main.py to master version

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
 import sys
-from intervaltree import Interval, IntervalTree
-import subtitles
-import srtparser
-import srt
-
-
-def main(argv):
-    if len(argv) < 2:
-        print("Usage python main.py filename")
-        sys.exit()
-
-    _, filename = argv
-
-    try:
-        subs = srtparser.parse_srt(filename)
-    except srt.SRTParseError:
-        print("Invalid Srt file")
-        sys.exit()
-
-    subs_set = subtitles.SubtitleSet(subs)
-
-    for s in subs_set.get_overlapping_subtitles(1000, 5000):
-        print(s.start_time())
-        print(s.end_time())
-        print(s.dialog())
-        s.set_dialog("Russ is a Genius")
-        s.update_interval(6000, 6100)
-
-
-    for s in subs_set.get_overlapping_subtitles(6100, 7000):
-        print(s.start_time())
-        print(s.end_time())
-        print(s.dialog())
+from PySide2.QtWidgets import QApplication, QLabel
 
 if __name__ == "__main__":
-    main(sys.argv)
+    app = QApplication([])
+    label = QLabel("Hello world")
+    label.show()
+    sys.exit(app.exec_())    
